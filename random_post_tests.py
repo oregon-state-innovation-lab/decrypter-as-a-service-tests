20167e9172ea3dedd7025b4fbe54f85721a47137
Alexis Deviney <70820575+adeviney@users.noreply.github.com>
2022-12-02 13:46:18 -0800

    added analog random POST tests to the random GET tests

---
+++ random_post_tests.py
@@ -0,0 +1,50 @@
+import requests
+import json
+from TestCases import TestCases
+
+
+endpoint = "<paste_your_endpoint_here>"
+
+def random_happy_path_tests(numberOfTests):
+    mytestcases = TestCases(numberOfTests) 
+    numTestsPassed = 0
+
+    for testNum, (sha_hash, password) in enumerate(mytestcases.tests.items()):
+        
+        print(f"Starting test number {testNum+1}.\n sha_hash: {sha_hash}")
+        myobj = {"sha_hash": sha_hash}
+
+
+        response = requests.post(endpoint, json=myobj)
+
+
+        try:
+            assert response.status_code == 200
+        
+        except:
+            print(f"Failed test due to status code. Expected 200; got {response.status_code}")
+            print("...")
+            continue
+
+        try:
+            assert (
+                json.loads(response.text).get(sha_hash) == password
+                or json.loads(response.text).get(sha_hash.upper()) == password
+            )
+
+            print(f"Test {testNum+1} passes!")
+            print("...")
+            numTestsPassed += 1
+
+        except:
+                print(f'Failed test {testNum+1} due to response text. Expected {{"{sha_hash}": "{password}"}}')
+                print(f"Got: {response.text}")
+                print("...")
+            
+    return numTestsPassed
+
+
+x = 10
+score = random_happy_path_tests(10)
+if score == x:
+    print("All tests pass. Yay!")
